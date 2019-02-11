053a6276675db2faf19243ab1af6b7b14797dcba
Emma Wyman <ejpink4@gmail.com>
2019-02-11 13:31:35 -0500

    Started project. Enumerated categories. Started the class List.

---
+++ GroList/List.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroList
+{
+    public class List
+    {
+        public DateTime Date { get; set; }
+        public Category Category { get; set; }
+    }
+
+    public enum Category
+    {
+        Produce,
+        Dairy,
+        Bakery,
+        Meat,
+        FrozenFood,
+        CannedFood,
+        Snacks,
+        GrainsCereal,
+        BakingCondiments,
+        Beverages,
+        PetSupplies,
+        PaperPlastic,
+        CleaningSupplies,
+        HealthBeauty,
+        Other
+    }
+}
