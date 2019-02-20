5ce10787e89bea5e8d6ff8fc9bc0127c7aa8d4da
Emma Wyman <ejpink4@gmail.com>
2019-02-20 15:04:57 -0500

    started planning menu.

--- GroList/List.cs
+++ GroList/List.cs
@@ -30,4 +30,5 @@ namespace GroList
         HealthBeauty,
         Other
     }
+
 }
--- GroList/Program.cs
+++ GroList/Program.cs
@@ -10,11 +10,48 @@ namespace GroList
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello! Please enter a category: ");
-            string inputCategory = Console.ReadLine();
+            Console.Write("Hello! Would you like to see the menu? Type YES to see the menu, type NO to quit:   ");
+             string input = Console.ReadLine();
 
-            Console.WriteLine(inputCategory);
+            Console.WriteLine(input);
+
+            //If statement to display main menu
+            if(input == "YES")
+            {
+                MainMenu();
+            }
+            else if(input == "NO")
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                //there has to be a more straightforward way of writing this out...
+                Console.Write("Please input YES or NO:   ");
+                string homeinput = Console.ReadLine();
+                if (homeinput == "YES")
+                { MainMenu(); }
+                else if (input == "NO")
+                { Environment.Exit(0); }
+                else
+                {
+                    Console.Write("Please input YES or NO:   ");
+                }
+            }
+            //My problem right now is that the program only goes through the if statement once, then anything else you type just closes the program.
+            //I need to be able to loop through the if statement until either the user types YES and goes to the main menu or they type NO and exit the program.
+
+            //Main menu needs to be selectable to print out other options.
+            //Categories option should print out a list of categories. 
+            //New list should start the user on creating a new list.
+            //Saved lists should print out saved lists sorted newest to oldest. Maybe they should be able to choose a date range or specific date?
+
             Console.Read();
+        }
+
+        private static void MainMenu()
+        {
+            Console.WriteLine("Categories, New List, Saved Lists");
         }
     }
 }
